3aeb7c5b22a10c6a923ccf457bc3e910ac558d22
Eunchae <ddungal3@gmail.com>
2023-07-14 15:30:54 +0900

    State 공부_ Counter2컴포넌트(Counter2.js)

---
+++ kidrauhl-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter2 from "./components/Counter2";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter2 />
+    </div>
+  );
+}
+
+export default App;
