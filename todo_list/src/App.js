f3cbb830d6a177dced41aa040b919ebfd1d9cd92
Eunchae <ddungal3@gmail.com>
2023-07-31 23:15:14 +0900

    todoList 데이터에서, 완료한 것만 보여주기

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,27 +1,53 @@
 
 function App() {
 
-  // todoList 배열 만들기
+  // todoList 데이터 만들기 (객체를 원소로 갖는 배열열)
   const todoList = [
-    " 방 청소하기",
-    "빨래하기",
-    "리액트 공부하기",
-    "헬스장 가기",
-    "산책 가기",
-    "책 읽기",
-    "러닝하기",
+    {
+      id: 0,
+      title: " 방 청소하기",
+      isChecked: false, /*todoList에서 완료한 데이터는 isChecked값이 true */
+    },
+    {
+      id: 1,
+      title: "빨래하기",
+      isChecked: true,
+    },
+    {
+      id: 2,
+      title: "리액트 공부하기",
+      isChecked: false,
+    },
+    {      
+      id: 3,
+      title: "헬스장 가기",
+      isChecked: true,
+    },
+    {
+      id: 4,
+      title: "산책 가기",
+      isChecked: false,
+    },
+    { 
+      id: 5,
+      title: "책 읽기",
+      isChecked:true,
+    },
   ];
 
+  // 배열의 filter메서드를 활용해, checkedList를 만든다.
+  const checkedList = todoList.filter((todoItem) => todoItem.isChecked);
 
-  // map을 활용해, 데이터를 react element가 담긴 todoItems로 변경
-  const todoItems = todoList.map((todoItem) => <li>{todoItem}</li>);
+  // map을 활용해, checkedItems를 만든다.
+  const checkedItems = checkedList.map((checkedItem) => (
+    <li>{checkedItem.title}</li>
+  ));
 
   return (
     <section>
-      <h1>나의 투두리스트</h1>
-      {/* 만들어진 todoItem을 ul 안에 넣기 */}
+      <h1>완료한 투두리스트</h1>
       <ul>
-        {todoItems}
+        {checkedItems}
       </ul>
     </section>
   );
